8d185c8b5fda4a875a034afcb81c22580bd9a2d9
aniketh <sunnyrockzzz1811@gmail.com>
2025-02-17 19:25:57 +0530

    Add Logs By Room/Emp Feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,41 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import './App.css'
 import LatestLogs from './components/latestLogs'
+import apiClient from './apiClient'
 
 
 function App() {
   const [count, setCount] = useState(0)
   const [employees, setEmployees] = useState([])
-  const [rooms, setRooms] = 
+  const [rooms, setRooms] = useState([])
 
   const navigate = useNavigate()
 
-  useEffect(()=>{
-    if(!localStorage.getItem('token')) navigate("/signup")
+  useEffect(() => {
+    if (!localStorage.getItem('token')) navigate("/signup")
+
+    async function getData() {
+      const empResponse = await apiClient.get('/employees', {
+        headers: { Authorization: 'Bearer ' + localStorage.getItem('token') }
+      })
+
+      const roomResponse = await apiClient.get('/rooms', {
+        headers: { Authorization: 'Bearer ' + localStorage.getItem('token') }
+      })
+
+      setEmployees(empResponse.data)
+      setRooms(roomResponse.data)
+    }
+
+    getData()
+
   }, [])
 
   return (<>
-  <LatestLogs />
+    <LatestLogs />
+    {employees.map((value, index)=><Link to={"/logs/emp/"+value.id} key={index}>{value.name}</Link>)}
+    {rooms.map((value, index)=><Link to={"/logs/room/"+value.id} key={index}>{value.name}</Link>)}
   </>)
 }
 
